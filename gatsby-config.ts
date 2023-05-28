3f0c636a6a39374445f036c9ec8f7c3275a003e3
PoorTable <poortable1337@gmail.com>
2023-05-28 12:20:42 +0200

    remove duplicated useless icon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -349,7 +349,7 @@ const config: GatsbyConfig = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: `src/images/icon.png`
+        icon: `static/favicon.png`
       }
     },
     {
